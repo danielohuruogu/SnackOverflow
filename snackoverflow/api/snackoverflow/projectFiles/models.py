0cd69501b8112f017ea15b927e056e530a74fdab
Deborah Stuart <timstu98@gmail.com>
2021-05-04 11:57:21 +0100

    MongoEngine working, tidied up code, added all models

--- snackoverflow/api/snackoverflow/projectFiles/models.py
+++ snackoverflow/api/snackoverflow/projectFiles/models.py
@@ -1,22 +1,30 @@
 import flask
 from snackoverflow import mongo
 import datetime
-from mongoengine import Document, ListField, StringField, URLField, DateTimeField
+from mongoengine import Document, ListField, StringField, URLField, DateTimeField, IntField
 
 class Topic(mongo.Document):
     # id = StringField()
     title = StringField(required=True)
     date_created = DateTimeField(default=datetime.datetime.utcnow)
-    posts_id = ListField(StringField())
+    posts_id = ListField(IntField())
 
     meta = {'collection': 'topics'}
+
+class Comment(mongo.Document):
+    # id = StringField()
+    text = StringField(required=True)
+    user_id = IntField(required=True)
+    popularity = IntField()
+
+    meta = {'collection': 'comments'}
 
 class User(mongo.Document):
     # id = StringField()
     username = StringField(required=True)
     password = StringField(required=True)
     email = StringField(required=True)
-    posts_id = ListField(StringField())
-    comments_id = ListField(StringField())
+    posts_id = ListField(IntField())
+    comments_id = ListField(IntField())
 
     meta = {'collection': 'users'}
