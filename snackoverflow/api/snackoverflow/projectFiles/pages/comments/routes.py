82d0a8fa7c56b874dc2542a94da1bd5fa21580bf
Deborah Stuart <timstu98@gmail.com>
2021-05-06 15:51:55 +0100

    Working POST routes for comments and posts

--- snackoverflow/api/snackoverflow/projectFiles/pages/comments/routes.py
+++ snackoverflow/api/snackoverflow/projectFiles/pages/comments/routes.py
@@ -3,6 +3,9 @@ from ...models import User, Post, Comment
 from .... import mongo
 import datetime
 from bson import ObjectId
+# import ..posts.routes
+import json
+from bson.json_util import loads
 
 comments = Blueprint('comments', __name__)
 
@@ -34,14 +37,17 @@ def update_popularity_comment(id):
     # return comment.to_json(), 200 #Correct code?
     return 'not working properly'
 
+
+'''
 @comments.route('/comments',methods=['POST'])
 def post_comment():
 
+    from ..posts.routes import get_one_post, put_post
+
     body = request.get_json()
-    print(body)
     if 'user_id' in body:
         user_id = list(body['user_id'].values())[0]
-        del body['user_id']
+        body['user_id'] = user_id
     else:
         return '',400
 
@@ -51,15 +57,41 @@ def post_comment():
     else:
         return '',400
 
-    print(body)
-    print(user_id)
-    print(post_id)
+    comment = Comment(**body).save()
+    comment_id = comment.id
 
-    # print(list(post_id.values())[0])
-
+    post = json.loads(get_one_post(post_id).data)
+    comments_ids = post['comments_id']
+    comments_ids.append(ObjectId(comment_id))
+    
+    put_post(post_id,data=post,comment=comment)
+    # Call put_post() with my new post as body
 
     # comment = Comment(**body).save()
-    # comment_id = comment.id
 
     # return {'new_comment_id': str(comment_id)}, 200
     return {},200
+'''
+
+@comments.route('/comments',methods=['POST'])
+def post_comment():
+
+    ## Unpack Data
+    all_data = request.get_json()
+    comment_text = all_data['text']
+    user_id = list(all_data['user_id'].values())[0]
+    post_id = list(all_data['post_id'].values())[0]
+
+    ## Find User to add to comment object
+    user = User.objects.get(id=user_id)
+
+    ## Post comment
+    comment = Comment(text=comment_text,user_id=user).save()
+    comment_id = comment.id
+    print(comment_id)
+
+    ## Find Post and add comment ref to it
+    Post.objects(id=post_id).update_one(push__comments_id=comment)
+
+
+    return {'new_comment_id': str(comment_id)}, 200
--- snackoverflow/api/snackoverflow/projectFiles/pages/posts/routes.py
+++ snackoverflow/api/snackoverflow/projectFiles/pages/posts/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, Response, jsonify
-from ...models import User, Post
+from ...models import User, Post, Topic
 from .... import mongo
 import datetime
 from bson import ObjectId
-from ..comments.routes import get_one_comment
+# import ..comments.routes as comments_fns
 import json
+# import requests
 
 posts = Blueprint('posts', __name__)
 
@@ -37,17 +38,19 @@ def update_posts(id):
     return post.to_json(), 200 #Correct code?
     # return 'not working properly'
 
-@posts.route('/posts',methods=['POST'])
-def post_post():
+# @posts.route('/posts',methods=['POST'])
+# def post_post():
 
-    body = request.get_json()
-    post = Post(**body).save()
-    id = post.id
+#     body = request.get_json()
+#     post = Post(**body).save()
+#     id = post.id
 
-    return {'id': str(id)}, 200
+#     return {'id': str(id)}, 200
 
 @posts.route('/posts/<id>/comments',methods=['GET'])
 def return_comments_for_post(id):
+
+    from ..comments.routes import get_one_comment
 
     ObjId = ObjectId(id)
     post = Post.objects.get(id=ObjId)
@@ -59,3 +62,66 @@ def return_comments_for_post(id):
 
     return jsonify(comments), 200
 
+'''
+@posts.route('/posts/<id>',methods=['PUT'])
+def put_post(id,data=None,comment=none):
+
+    ObjId = ObjectId(id)
+
+    # Checks if it has been passed data - only passed data if being called inside another route
+    if data:
+        body = data
+    else:
+        body = request.get_json()
+
+    print(body)
+
+    #Seems to mess up the put request a bit
+    # if '_id' in body:
+    #     del body['_id']
+
+    # To convert all $oid to int's.
+    if 'comments_id' in body:
+        int_comments_id = []
+        for item in body['comments_id']:
+            int_comments_id.append(item['$oid'])
+        body['comments_id'] = int_comments_id
+
+    # del body['title']
+    del body['user_id']
+    # del body['post_body']
+
+    # post  = Post.objects.get(id=ObjId).update(**body)
+    print(body)
+    post  = Post.objects.get(id=ObjId)
+    print(post.to_json())
+    # post = post.update(**body)
+
+    post = post.update(comments_id=body['comments_id'])
+
+    return '', 200
+    # return post.to_json(), 200
+'''
+
+@posts.route('/posts',methods=['POST'])
+def post_post():
+
+    ## Unpack Data
+    all_data = request.get_json()
+    post_title = all_data['title']
+    post_body = all_data['post_body']
+    user_id = list(all_data['user_id'].values())[0]
+    topic_id = list(all_data['topic_id'].values())[0]
+
+    ## Find User to add to comment object
+    user = User.objects.get(id=user_id)
+
+    ## Post comment
+    post = Post(title=post_title,post_body=post_body,user_id=user).save()
+    post_id = post.id
+    print(post_id)
+
+    ## Find Post and add comment ref to it
+    Topic.objects(id=topic_id).update_one(push__posts_id=post)
+
+    return {'new_post_id': str(post_id)}, 200
